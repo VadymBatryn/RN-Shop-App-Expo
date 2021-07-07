567f53bfe412415393bdabaed6ffa923afdc6464
VadymBatryn <vadimbatrin4@gmail.com>
2021-07-07 09:53:12 +0300

    feat: add load indicator and empty screen cmp

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -1,5 +1,12 @@
-import React from 'react';
-import { StyleSheet, FlatList, View, Button, Text } from 'react-native';
+import React, { useState } from 'react';
+import {
+	StyleSheet,
+	FlatList,
+	View,
+	Button,
+	Text,
+	ActivityIndicator,
+} from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 
 import * as ordersAction from '../../store/actions/orders';
@@ -7,8 +14,11 @@ import * as cartAction from '../../store/actions/cart';
 import Colors from '../../constants/colors';
 import CartItem from '../../components/shop/CartItem';
 import Card from '../../components/UI/Card';
+import EmptyPage from '../../components/UI/EmptyPage';
 
 export default function CartScreen(props) {
+	const [isLoading, setIsLoading] = useState(false);
+
 	//Just a dispatch
 	const dispatch = useDispatch();
 
@@ -32,6 +42,23 @@ export default function CartScreen(props) {
 		);
 	});
 
+	const sendOrderHandler = async () => {
+		setIsLoading(true);
+		await dispatch(ordersAction.addOrder(cartItems, cardTotalAmount));
+		setIsLoading(false);
+	};
+
+	if (!isLoading && cartItems.length === 0) {
+		return (
+			<EmptyPage
+				iconName='ios-cart'
+				buttonTitle='Back to Shop'
+				text={`Nothing Here. ${'\n'}Start shopping`}
+				onPressHandler={() => props.navigation.goBack()}
+			/>
+		);
+	}
+
 	return (
 		<View style={styles.screen}>
 			<Card style={styles.summary}>
@@ -41,46 +68,38 @@ export default function CartScreen(props) {
 						${Math.round(cardTotalAmount.toFixed(2) * 100) / 100}
 					</Text>
 				</Text>
-				<Button
-					color={Colors.accent}
-					title='Order Now'
-					disabled={cartItems.length === 0}
-					onPress={() => {
-						dispatch(ordersAction.addOrder(cartItems, cardTotalAmount));
-					}}
-				/>
-			</Card>
-			{cartItems.length > 0 ? (
-				<FlatList
-					style={{
-						borderRadius: 10,
-						marginHorizontal: 20,
-					}}
-					data={cartItems}
-					keyExtractor={(item) => item.productId}
-					renderItem={(itemData) => (
-						<CartItem
-							quantity={itemData.item.quantity}
-							title={itemData.item.productTitle}
-							amount={itemData.item.sum}
-							deletable={true}
-							onRemove={() => {
-								dispatch(cartAction.removeFromCart(itemData.item.productId));
-							}}
-						/>
-					)}
-				/>
-			) : (
-				<View style={styles.emptyCartContainer}>
-					<Text style={styles.emptyCartText}>
-						Nothing Here. {'\n'}Start shopping
-					</Text>
+				{isLoading ? (
+					<View style={styles.centered}>
+						<ActivityIndicator size='small' color={Colors.primary} />
+					</View>
+				) : (
 					<Button
-						title='Back to Shop'
-						onPress={() => props.navigation.goBack()}
+						color={Colors.accent}
+						title='Order Now'
+						disabled={cartItems.length === 0}
+						onPress={sendOrderHandler}
 					/>
-				</View>
-			)}
+				)}
+			</Card>
+			<FlatList
+				style={{
+					borderRadius: 10,
+					marginHorizontal: 20,
+				}}
+				data={cartItems}
+				keyExtractor={(item) => item.productId}
+				renderItem={(itemData) => (
+					<CartItem
+						quantity={itemData.item.quantity}
+						title={itemData.item.productTitle}
+						amount={itemData.item.sum}
+						deletable={true}
+						onRemove={() => {
+							dispatch(cartAction.removeFromCart(itemData.item.productId));
+						}}
+					/>
+				)}
+			/>
 		</View>
 	);
 }
@@ -106,15 +125,5 @@ const styles = StyleSheet.create({
 	},
 	amount: {
 		color: Colors.primary,
-	},
-	emptyCartContainer: {
-		justifyContent: 'center',
-		height: '85%',
-	},
-	emptyCartText: {
-		fontSize: 30,
-		fontFamily: 'open-sans-bold',
-		textAlign: 'center',
-		color: '#ccc',
 	},
 });
