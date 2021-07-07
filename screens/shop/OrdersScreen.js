97a50eaca465e54d29b0cc884524e1e3902bc204
VadymBatryn <vadimbatrin4@gmail.com>
2021-07-07 10:08:36 +0300

    feat: fetching orders when entering the screen

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { FlatList, ActivityIndicator, View, StyleSheet } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
@@ -11,15 +11,47 @@ import Colors from '../../constants/colors';
 
 export default function OrdersScreen(props) {
 	const [isLoading, setIsLoading] = useState(false);
+	const [isRefreshing, setIsRefreshing] = useState(false);
+	const [errorMessage, setErrorMessage] = useState('');
 
 	const orders = useSelector((state) => state.orders.orders);
 
 	const dispatch = useDispatch();
 
+	const loadOrders = useCallback(async () => {
+		setErrorMessage(null);
+		setIsRefreshing(true);
+		try {
+			await dispatch(ordersActions.fetchOrders());
+		} catch (err) {
+			setErrorMessage(err.message);
+		}
+		setIsRefreshing(false);
+	}, [dispatch, setErrorMessage, setIsLoading]);
+
+	useEffect(() => {
+		const willFocusSub = props.navigation.addListener('willFocus', loadOrders);
+
+		return () => {
+			willFocusSub.remove();
+		};
+	}, [loadOrders]);
+
 	useEffect(() => {
 		setIsLoading(true);
 		dispatch(ordersActions.fetchOrders()).then(() => setIsLoading(false));
 	}, [dispatch]);
+
+	if (errorMessage) {
+		return (
+			<EmptyPage
+				iconName='ios-bug-outline'
+				buttonTitle='Try again'
+				text={`An error occured!`}
+				onPressHandler={() => loadOrders()}
+			/>
+		);
+	}
 
 	if (isLoading) {
 		return (
@@ -42,6 +74,8 @@ export default function OrdersScreen(props) {
 
 	return (
 		<FlatList
+			onRefresh={loadOrders}
+			refreshing={isRefreshing}
 			data={orders}
 			keyExtractor={(item) => item.id}
 			renderItem={(itemData) => (
