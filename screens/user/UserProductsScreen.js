57ff23846ee92367895ec3c0e586012082b2a5b6
VadymBatryn <vadimbatrin4@gmail.com>
2021-07-07 10:12:47 +0300

    feat: add loading / error / empty screen

--- screens/user/UserProductsScreen.js
+++ screens/user/UserProductsScreen.js
@@ -1,5 +1,13 @@
-import React from 'react';
-import { FlatList, Platform, Button, Alert } from 'react-native';
+import React, { useState, useEffect, useCallback } from 'react';
+import {
+	FlatList,
+	Platform,
+	Button,
+	Alert,
+	View,
+	ActivityIndicator,
+	StyleSheet,
+} from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -7,30 +15,72 @@ import ProductItem from '../../components/shop/ProductItem';
 import CustomHeaderButton from '../../components/UI/HeaderButton';
 import Colors from '../../constants/colors';
 import * as ProductsActions from '../../store/actions/products';
+import EmptyPage from '../../components/UI/EmptyPage';
 
 export default function UserProductsScreen(props) {
+	const [errorMessage, setErrorMessage] = useState('');
+	const [isLoading, setIsLoading] = useState(false);
+
 	const userProducts = useSelector((state) => state.products.userProducts);
 
 	const dispatch = useDispatch();
 
-	const deleteHandler = (productId) => {
-		Alert.alert('Are you sure?', 'Do you really wanna delete this product? ', [
-			{
-				text: 'No',
-				style: 'default',
-			},
-			{
-				text: 'Yes',
-				style: 'destructive',
-				onPress: () => {
-					dispatch(ProductsActions.deleteProduct(productId));
-				},
-			},
-		]);
-	};
-
 	const editProductHandler = (id) =>
 		props.navigation.navigate('EditProduct', { productId: id });
+
+	const deleteHandler = useCallback(
+		(productId) => {
+			setErrorMessage(null);
+			Alert.alert(
+				'Are you sure?',
+				'Do you really wanna delete this product? ',
+				[
+					{
+						text: 'No',
+						style: 'default',
+					},
+					{
+						text: 'Yes',
+						style: 'destructive',
+						onPress: async () => {
+							setIsLoading(true);
+							await dispatch(ProductsActions.deleteProduct(productId))
+								.catch((error) => {
+									setErrorMessage(error.message);
+								})
+								.finally(() => setIsLoading(false));
+						},
+					},
+				]
+			);
+		},
+		[dispatch, userProducts]
+	);
+
+	useEffect(() => {
+		if (errorMessage) {
+			Alert.alert('An error occured!', errorMessage, [{ text: 'Okay' }]);
+		}
+	}, [errorMessage]);
+
+	if (isLoading) {
+		return (
+			<View style={styles.centered}>
+				<ActivityIndicator size='large' color={Colors.primary} />
+			</View>
+		);
+	}
+
+	if (!isLoading && userProducts.length === 0) {
+		return (
+			<EmptyPage
+				iconName='ios-add-circle'
+				buttonTitle='Add Product'
+				text={`Nothing here...${'\n'}Lets add something!`}
+				onPressHandler={() => props.navigation.navigate('EditProduct')}
+			/>
+		);
+	}
 
 	return (
 		<FlatList
@@ -57,6 +107,14 @@ export default function UserProductsScreen(props) {
 		/>
 	);
 }
+
+const styles = StyleSheet.create({
+	centered: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+});
 
 UserProductsScreen.navigationOptions = (navData) => {
 	return {
